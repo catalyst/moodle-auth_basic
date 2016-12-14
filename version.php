be8648e4eec2645e1469682bf6fbbbfc254b0c9c
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2016-12-14 19:10:02 +1100

    Version bump to 2016101001.

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015113001;    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2015113001;    // Match release exactly to version.
+$plugin->version   = 2016101001;    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2016101001;    // Match release exactly to version.
 $plugin->requires  = 2014050800;    // Requires this Moodle version.
 $plugin->component = 'auth_basic';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
