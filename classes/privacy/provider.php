1ba268b746a5fa542f1a229dd1a34b4b3ea52965
Nathan Nguyen <nathannguyen@catalyst-au.net>
2019-04-23 16:06:52 +1000

    WR#310004 - privacy provider, use polyfill to support multiple branch

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -38,13 +38,15 @@ class provider implements
     \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\request\plugin\provider {
 
+    use \core_privacy\local\legacy_polyfill;
+
     /**
      * Returns meta data about this system.
      *
      * @param   collection $collection The initialised collection to add items to.
      * @return  collection     A listing of user data stored through this system.
      */
-    public static function get_metadata(collection $collection) {
+    public static function _get_metadata(collection $collection) {
         $collection->add_database_table('auth_basic_master_password', [
             'userid' => 'privacy:metadata:auth_basic_master_password:userid',
         ], 'privacy:metadata:auth_basic_master_password');
@@ -58,7 +60,7 @@ class provider implements
      * @param   int $userid The user to search.
      * @return  contextlist   $contextlist  The contextlist containing the list of contexts used in this plugin.
      */
-    public static function get_contexts_for_userid(int $userid) {
+    public static function _get_contexts_for_userid(int $userid) {
         $contextlist = new contextlist();
 
         $sql = "SELECT c.id
