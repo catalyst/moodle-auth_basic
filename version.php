7c81f764a69b680b67217153ae223f02b5f646fa
almadog <nbozhkov@hotmail.com>
2022-03-15 09:54:52 +0000

    version bump

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020120300;    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2020101300;    // Match release exactly to version.
+$plugin->version   = 2021073000;    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2021073000;    // Match release exactly to version.
 $plugin->requires  = 2014050800;    // Requires this Moodle version.
 $plugin->component = 'auth_basic';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
